2a1666018d5ee226ddb273ec775c3c24c9757a1e
MB3R3 <essienmbereidem@gmail.com>
2022-09-12 15:31:13 +0200

    print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,3 +7,10 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
     except IndexError:
         pass
+    finally:
+        if n < x:
+            print('')
+            return n
+        else:
+            print('')
+            return x
