57237e7a9aea7fdc6115a9aa89f85b08df4d7419
MB3R3 <essienmbereidem@gmail.com>
2022-09-20 12:50:59 +0200

    test add_integer func

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,12 +5,12 @@ This modude has one function that adds two integers
 
 """
 
+
 def add_integer(a, b=98):
     """
-    a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
+    a and b must be integers or floats, otherwise raise a TypeError exception
     a and b must be first casted to integers if they are float
     Returns an integer: the addition of a and b
-    You are not allowed to import any module
     """
     if isinstance(a, float):
         a = int(a)
@@ -20,6 +20,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not (isinstance(b, float) or isinstance(b, int)):
         raise TypeError("b must be an integer")
-
-
     return a + b
