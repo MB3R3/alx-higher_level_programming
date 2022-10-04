b7abdae2d9f7da1b6accfd3924306211f8f94c5b
MB3R3 <essienmbereidem@gmail.com>
2022-10-04 18:53:00 +0200

    function to list obj attributes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,6 +5,7 @@ This project module contains one method that returns the
 list of available attributes and methods of an object.
 """
 
+
 def lookup(obj):
     """
     This is a method that returns the list of
