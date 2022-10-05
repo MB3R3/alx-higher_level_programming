9ffde180068da96baa17aec12606f5127f204f7c
MB3R3 <essienmbereidem@gmail.com>
2022-10-05 20:41:40 +0200

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Task 0
+This is a function that prints
+the contents of a file
+with utf-8 encoding
+"""
+
+
+def read_file(filename=""):
+    """
+    the file - 'filename is read by
+    thw function read_file
+
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())
