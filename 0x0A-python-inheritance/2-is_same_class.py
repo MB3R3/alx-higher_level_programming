2fe293bfdd45835c6adaacaaa3f78c17e21e464c
MB3R3 <essienmbereidem@gmail.com>
2022-10-05 11:11:31 +0200

    obj and class comparism

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
+
 """
 Task 2
-a function that returns True if the 
-object is exactly an instance of 
+a function that returns True if the
+object is exactly an instance of
 the specified class, otherwise False.
 """
+
+
 def is_same_class(obj, a_class):
+
     """
     function def is_same_class(obj, a_class):
     checks if the object(obj) is an instance of the
     class (a_class)
     Using the type operator we can check
     """
+
     if type(obj) is a_class:
         return True
     else:
