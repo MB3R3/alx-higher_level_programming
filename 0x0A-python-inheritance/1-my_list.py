b1dafca8a9fa1469cbd486d2127e169ad9a1998a
MB3R3 <essienmbereidem@gmail.com>
2022-10-05 10:54:58 +0200

    Added docstrings

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python3
 
-class MyList(list):
+"""
+Task 1.
+Write a class MyList that inherits from list.
+Public instance method: def print_sorted(self).
+Prints the list, but sorted (ascending sort).
+"""
 
-     def print_sorted(self):
-         print(sorted(self))
+
+class MyList(list):
+    """
+    class MyList in which def print_sorted(self) is
+    defined as Public instance method.
+    Args:
+        list: Super class.
+    Methods:
+        def print_sorted(self):
+    """
+    def print_sorted(self):
+        """
+        Print method.
+        Prints the list, but sorted (ascending sort).
+        Assume that all the elements of the list will
+        be of type int.
+        """
+        print(sorted(self))
