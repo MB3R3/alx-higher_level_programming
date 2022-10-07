ed6077fef14db7db0a134bf364d56fe713861973
MB3R3 <essienmbereidem@gmail.com>
2022-10-07 05:24:09 +0200

    base updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+"""
+class Base
+with private class attribute
+__nb_objects
+"""
 class Base:
 
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initialization function.
+        If id is not None, assign the public instance
+        attribute id with this argument value. Otherwise,
+        increment __nb_objects and assign the new value
+        to the public instance attribute id.
+        Attributes:
+            id: Number of identification.
+        """
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
